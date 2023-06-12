0836488e5dc45e5ac50a4fe9d362bab9f939fee9
AmigoDeTomas <130453053+AmigoDeTomas@users.noreply.github.com>
2023-06-12 09:39:36 +0200

    m

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
-    basic.showString("ºC")
-    basic.clearScreen()
+	
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(input.temperature() + 32)
-    basic.showString("ºF")
-    basic.clearScreen()
+	
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.temperature() + 273)
-    basic.showString(" K")
-    basic.clearScreen()
+	
 })
 let Lectura = 0
 radio.setGroup(1)
+basic.forever(function () {
+    basic.showString("temp")
+    basic.showNumber(Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1))
+    basic.showString("ºC")
+    basic.pause(100)
+    basic.showString("hum")
+    basic.showNumber(Environment.dht11value(Environment.DHT11Type.DHT11_humidity, DigitalPin.P1))
+    basic.showString("g")
+    basic.pause(100)
+    if (Environment.dht11value(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P1) >= 25) {
+        music.playMelody("C5 G B A F A C5 B ", 120)
+    }
+})
 basic.forever(function () {
     Lectura = pins.analogReadPin(AnalogPin.P0)
     led.plotBarGraph(
